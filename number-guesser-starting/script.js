2e216c2fdedc9628c10bd87bda97d4e32bca02e8
coraliniwafuchi <coraliniwafuchi@gmail.com>
2024-02-22 15:25:35 +0900

    Finishing the JS file

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -3,4 +3,26 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = (num) => {
+  num = Math.floor(Math.random() * 9);
+  return num;
+};
 
+const compareGuesses = (humanGuess, compGuess, secretTarget) => {
+  const humanDifference = Math.abs(secretTarget - humanGuess);
+  const computerDifference = Math.abs(secretTarget - compGuess);
+  return humanDifference <= computerDifference;
+};
+const updateScore = (winner) => {
+  if ("human" === winner) {
+    humanScore++;
+  }
+
+  if ("computer" === winner) {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber++;
+};
